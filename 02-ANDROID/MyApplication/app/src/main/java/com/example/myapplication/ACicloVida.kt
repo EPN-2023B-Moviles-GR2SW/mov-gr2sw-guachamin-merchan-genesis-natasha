9e3d51aff5f2c508d9c97c6145efbf21105c8df6
GenesisNGuachamin <genesis.guachamin@epn.edu.ec>
2023-12-06 09:05:51 -0500

    Actualización de app

---
+++ 02-ANDROID/MyApplication/app/src/main/java/com/example/myapplication/ACicloVida.kt
@@ -0,0 +1,78 @@
+package com.example.myapplication
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Button
+import androidx.compose.material3.Snackbar
+import com.google.android.material.snackbar.Snackbar
+
+
+class ACicloVida : AppCompatActivity() {
+    var textoGlobal = ""
+
+    fun mostrarSnackbar(texto:String){
+        textoGlobal += texto
+        val snack = Snackbar.make(findViewById(R.id.cl_ciclo_vida),
+            textoGlobal, Snackbar.LENGTH_INDEFINITE)
+        snack.show()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_aciclo_vida)
+        mostrarSnackbar("Hola")
+        mostrarSnackbar("OnCrete")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mostrarSnackbar("onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mostrarSnackbar("onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mostrarSnackbar("onResume")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mostrarSnackbar("onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mostrarSnackbar("onDestroy")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.run{
+            putString("TextoGuardado", textoGlobal)
+        }
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle){
+        super.onRestoreInstanceState(savedInstanceState)
+        //RECUPERAR VARIABLES
+        //PRIMITIVAS
+
+        val textoRecuperado:String? = savedInstanceState
+            .getString("Texto Guardado")
+        if(textoRecuperado != null){
+            mostrarSnackbar(textoRecuperado)
+            textoGlobal = textoRecuperado
+        }
+    }
+
+
+
+
+
+}

aa5f9a78ad8a458507c71a551834650ed587b630
GenesisNGuachamin <genesis.guachamin@epn.edu.ec>
2024-01-17 09:58:14 -0500

    Clase 19 - Google Maps

--- 02-ANDROID/app/build.gradle.kts
+++ 02-ANDROID/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
 }
 
 android {
@@ -33,6 +34,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -50,6 +54,7 @@ dependencies {
     implementation("com.google.android.material:material:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("com.google.android.gms:play-services-maps:18.1.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- 02-ANDROID/app/src/main/java/com/example/b2023gr2sw/MainActivity.kt
+++ 02-ANDROID/app/src/main/java/com/example/b2023gr2sw/MainActivity.kt
@@ -108,6 +108,10 @@ class MainActivity : AppCompatActivity() {
                 irActividad(FRecyclerView::class.java)
             }
 
+        val botonGoogleMaps = findViewById<Button>(R.id.btn_google_maps)
+        botonGoogleMaps.setOnClickListener {
+            irActividad(GGoogleMapsActivity::class.java)
+        }
     }
 
     fun abrirActividadConParametros(

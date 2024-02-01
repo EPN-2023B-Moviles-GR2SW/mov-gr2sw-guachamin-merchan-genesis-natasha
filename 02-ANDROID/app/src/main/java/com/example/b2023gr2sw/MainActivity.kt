b194cba7cdb2ea6b03c6d517d905728db0af9c49
GenesisNGuachamin <genesis.guachamin@epn.edu.ec>
2024-02-01 01:29:52 -0500

    Clase 22 - FireStore

--- 02-ANDROID/app/src/main/java/com/example/b2023gr2sw/MainActivity.kt
+++ 02-ANDROID/app/src/main/java/com/example/b2023gr2sw/MainActivity.kt
@@ -118,6 +118,12 @@ class MainActivity : AppCompatActivity() {
             .setOnClickListener {
                 irActividad(HFirebaseUIAuth::class.java)
             }
+
+        val botonFirestore = findViewById<Button>(R.id.btn_intent_firestore)
+        botonFirestore
+            .setOnClickListener {
+                irActividad(IFirestore::class.java)
+            }
     }
 
     fun abrirActividadConParametros(
